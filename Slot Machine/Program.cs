ae3eff694dae7841ed2165237a4f69a64b732bf8
Kiwicase <emailcaseydent@gmail.com>
2022-08-08 10:31:12 +0300

    Slot Machine Welcome Message

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HangmanGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row1 = 0;
+            int row2 = 0;
+            int row3 = 0;
+            Console.WriteLine("Welcome to Casey's Supremely Awesome Slot Machine!");
+
+        }
+    }
+}
