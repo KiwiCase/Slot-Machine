5a6e8027ce69ebe0787c38d6dcc2af9a7ffcdf06
Kiwicase <emailcaseydent@gmail.com>
2022-08-08 13:14:59 +0300

    Row and Column constants defined

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Casey's Supremely Awesome Slot Machine!");
+            int rowValue = 0;
+            int colValue = 0;
             Random rnd = new Random();
             int[,] slotNumber = new int[3, 3];
             for (int i = 0; i < 3; i++)
