f6ba1f09ffc3a67866b5210b563950c3b1679d83
Kiwicase <emailcaseydent@gmail.com>
2022-08-09 16:59:16 +0300

    Adding game instructions

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -4,20 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Casey's Supremely Awesome Slot Machine!");
-            int rowValue = 0;
-            int colValue = 0;
+
+            int cash = 50;
+            double lines;
             Random rnd = new Random();
             int[,] slotNumber = new int[3, 3];
-            for (int i = 0; i < 3; i++)
+  
+            Console.WriteLine("Welcome to Casey's Supremely Awesome Slot Machine!");
+            Console.WriteLine($"You currently have ${cash} left.\nHow many lines would you like to play? Choose 1 to 8 lines to play.");
+            while (!double.TryParse(Console.ReadLine(), out lines))
             {
-                for (int j = 0; j < 3; j++)
+                Console.WriteLine("This is not a valid input. Please choose 1 to 8 lines to play.");
+            }
+
+            Console.WriteLine("Press Enter to Start...");
+
+
+            for (int i = 0; i < slotNumber.GetLength(0); i++)
+            {
+                for (int j = 0; j < slotNumber.GetLength(1); j++)
                 {
                     slotNumber[i, j] = rnd.Next(0, 9);
                     Console.Write("{0}\t", slotNumber[i, j]);
+                }
+                Console.Write("\n\n");
+                while (cash > 0)
+                {
+                    if (slotNumber[0, 0] == slotNumber[0, 1] && slotNumber[0, 0] == slotNumber[0, 2])
+                        Console.WriteLine("YOU WIN!");
+                    else break;
                 }
             }
         }
 
     }
+
 }
