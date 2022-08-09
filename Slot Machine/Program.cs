a332d70369c7eea6f9f00b80d94868c1321a12b3
Kiwicase <emailcaseydent@gmail.com>
2022-08-09 17:35:20 +0300

    Select lines and bet per lines added

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -4,35 +4,52 @@
     {
         static void Main(string[] args)
         {
-
+            int bet = 0;
             int cash = 50;
             int lines = 0;
             Random rnd = new Random();
             int[,] slotNumber = new int[3, 3];
 
             Console.WriteLine("Welcome to Casey's Supremely Awesome Slot Machine!");
-            Console.WriteLine($"You currently have ${cash} left.\nHow many lines would you like to play? Choose 1 to 8 lines to play.");
-            Int32.TryParse(Console.ReadLine(), out lines);
-            if (lines > 8 || lines < 0)
-                Console.WriteLine("This is not a valid input. Please choose 1 to 8 lines to play");
-
-            for (int i = 0; i < slotNumber.GetLength(0); i++)
+            while (cash > 0)
             {
-                for (int j = 0; j < slotNumber.GetLength(1); j++)
+                Console.WriteLine($"You currently have ${cash} left.\nHow many lines would you like to play? Choose 1 to 8 lines to play: ");
+                while (Int32.TryParse(Console.ReadLine(), out lines))
                 {
-                    slotNumber[i, j] = rnd.Next(0, 9);
-                    Console.Write("{0}\t", slotNumber[i, j]);
-                }
-                Console.Write("\n\n");
-                while (cash > 0)
-                {
-                    if (slotNumber[0, 0] == slotNumber[0, 1] && slotNumber[0, 0] == slotNumber[0, 2])
-                        Console.WriteLine("YOU WIN!");
+                    if (lines > 8 || lines < 0)
+                    {
+                        Console.WriteLine("This is not a valid input. Please choose 1 to 8 lines to play: ");
+                    }
                     else break;
                 }
+
+                Console.WriteLine("How many dollars per line would you like to bet? Maximum $3 per line: ");
+                while (Int32.TryParse(Console.ReadLine(), out bet))
+                {
+                    if (bet > 3 || lines < 0)
+                    {
+                        Console.WriteLine("This is not a valid input. Please choose $1 to $3 per line: ");
+                    }
+                    else break;
+                }
+                for (int i = 0; i < slotNumber.GetLength(0); i++)
+                {
+                    for (int j = 0; j < slotNumber.GetLength(1); j++)
+                    {
+                        slotNumber[i, j] = rnd.Next(0, 9);
+                        Console.Write("{0}\t", slotNumber[i, j]);
+                    }
+                    Console.Write("\n\n");
+                    while (cash > 0)
+                    {
+                        if (slotNumber[0, 0] == slotNumber[0, 1] && slotNumber[0, 0] == slotNumber[0, 2])
+                            Console.WriteLine("YOU WIN!");
+                        else break;
+                    }
+                }
             }
+
         }
 
     }
-
 }
