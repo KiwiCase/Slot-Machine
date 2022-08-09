6546369bca0df384751837e277cf60851c3b8415
Kiwicase <emailcaseydent@gmail.com>
2022-08-09 17:53:53 +0300

    Fixing Lines and Bets per line

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -5,20 +5,20 @@
         static void Main(string[] args)
         {
             int bet = 0;
-            int cash = 50;
-            int lines = 0;
+            int cash = 25;
+            double lines = 0;
             Random rnd = new Random();
             int[,] slotNumber = new int[3, 3];
 
             Console.WriteLine("Welcome to Casey's Supremely Awesome Slot Machine!");
             while (cash > 0)
             {
-                Console.WriteLine($"You currently have ${cash} left.\nHow many lines would you like to play? Choose 1 to 8 lines to play: ");
-                while (Int32.TryParse(Console.ReadLine(), out lines))
+                Console.WriteLine($"You currently have ${cash}.\nHow many lines would you like to play? Choose 1 to 8 lines to play: ");
+                while (double.TryParse(Console.ReadLine(), out lines))
                 {
                     if (lines > 8 || lines < 0)
                     {
-                        Console.WriteLine("This is not a valid input. Please choose 1 to 8 lines to play: ");
+                        Console.WriteLine("This is not a valid input.\nPlease choose 1 to 8 lines to play: ");
                     }
                     else break;
                 }
@@ -28,10 +28,14 @@
                 {
                     if (bet > 3 || lines < 0)
                     {
-                        Console.WriteLine("This is not a valid input. Please choose $1 to $3 per line: ");
+                        Console.WriteLine("This is not a valid input.\nPlease choose $1 to $3 per line: ");
                     }
                     else break;
                 }
+
+                Console.WriteLine($"You are playing {lines} lines and betting ${bet} per line. Press Enter To Spin... ");
+                string keyChoice = Console.ReadLine();
+
                 for (int i = 0; i < slotNumber.GetLength(0); i++)
                 {
                     for (int j = 0; j < slotNumber.GetLength(1); j++)
