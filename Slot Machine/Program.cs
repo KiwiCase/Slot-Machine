7cbfb172fa29fc441c10fb99b3df8eb1af335913
Kiwicase <emailcaseydent@gmail.com>
2022-08-10 17:09:03 +0300

    Added multiplier method

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -11,7 +11,7 @@
             Random rnd = new Random();
             int[,] slotNumber = new int[3, 3];
 
-            Console.WriteLine("Welcome to Casey's Supremely Awesome Slot Machine!");
+            2Console.WriteLine("Welcome to Casey's Supremely Awesome Slot Machine!");
             while (cash > 0)
             {
                 Console.WriteLine($"You currently have ${cash}.\nHow many lines would you like to play? Choose 1 to 8 lines to play: ");
@@ -32,9 +32,19 @@
                     }
                     break;
                 }
-                break;
             }
-            Console.WriteLine($"You are playing {lines} {(lines > 1 ? "Lines" : "line")} and betting ${bet} per line. Press Enter To Spin... ");
+            static int multiplier(int lines, int bet)
+            {
+                int result = lines * bet;
+                return result;
+            }
+            if (lines > 1)
+            {
+                cash -= multiplier(lines, bet);
+            }
+            else cash -= bet;
+            Console.WriteLine(cash);
+            Console.WriteLine($"You are playing {lines} {(lines > 1 ? "Lines" : "line")} and betting ${bet} per line for a total of ${lines * bet}. Press Enter To Spin... ");
             string keyChoice = Console.ReadLine();
 
             for (int i = 0; i < slotNumber.GetLength(0); i++)
