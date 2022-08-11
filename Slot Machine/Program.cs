be3172e845f9ffb3965d6c84043e6b3da686eff5
Kiwicase <emailcaseydent@gmail.com>
2022-08-11 07:48:34 +0300

    cashCheck method added

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -5,11 +5,22 @@
         static void Main(string[] args)
         {
             int bet = 0;
-            int cash = 25;
+            int cash = 2;
             int lines = 0;
             int singleSlotNumber = 0;
             Random rnd = new Random();
             int[,] slotNumber = new int[3, 3];
+
+            static int cashCheck(int lines, int cash)
+            {
+                int result = lines * 1;
+                return result;
+            }
+            static int multiplier(int lines, int bet)
+            {
+                int result = lines * bet;
+                return result;
+            }
 
             Console.WriteLine("Welcome to Casey's Supremely Awesome Slot Machine!");
             while (cash > 0)
@@ -21,9 +32,14 @@
                     Console.WriteLine("This is not a valid input.\nPlease choose 1 to 8 lines to play: ");
                     input = Console.ReadLine();
                 }
+                while ((lines * 1) > cash)
+                {
+                    Console.WriteLine($"You do not have enough cash to bet this many lines. You currently have ${cash} - please choose less lines to play.\nHow many lines would you like to play?");
+                    input = Console.ReadLine();
+                }
                 if (lines < 8 || lines > 0)
                 {
-                    Console.WriteLine($"You are playing {lines} {(lines > 1 ? "Lines" : "line")}...\nHow many dollars per line would you like to bet? Maximum $3 per line: ");
+                    Console.WriteLine($"You are playing {lines} {(lines > 1 ? "Lines" : "line")}...\nHow many dollars per line would you like to bet?\nMinumum $1 per line\nMaximum $3 per line: ");
                     input = Console.ReadLine();
                     while (!Int32.TryParse(input, out bet) || bet > 3 || bet <= 0)
                     {
@@ -33,11 +49,7 @@
                     break;
                 }
             }
-            static int multiplier(int lines, int bet)
-            {
-                int result = lines * bet;
-                return result;
-            }
+
             if (lines > 1)
             {
                 cash -= multiplier(lines, bet);
