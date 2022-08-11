04f6f0059f33b7601eeeaddf934aa4d77f019cf0
Kiwicase <emailcaseydent@gmail.com>
2022-08-11 14:48:14 +0300

    Removed multiplier methiod, added betting returns and line checks

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             int bet = 0;
-            int cash = 2;
+            int cash = 25;
             int lines = 0;
             int singleSlotNumber = 0;
             Random rnd = new Random();
@@ -36,37 +36,116 @@
                         Console.WriteLine("This is not a valid input - please choose how many dollars per line you would like to bet -\nMinimum $1 per line\nMaximum $3 per line:  ");
                         input = Console.ReadLine();
                     }
-                    if ((bet * lines) > cash)
+                    while ((bet * lines) > cash)
                     {
                         Console.WriteLine($"You do not have enough cash to bet this amount per line. You currently have ${cash} - please choose a less bet per line.\nMinimum $1 per line\nMaximum $3 per line: ");
+                        input = Console.ReadLine();
+                        Int32.TryParse(input, out bet);
                     }
+                }
+
+                Console.WriteLine($"You are playing {lines} {(lines > 1 ? "lines" : "line")} and betting ${bet} per line for a total of ${lines * bet}. Press Enter To Spin... ");
+                string keyChoice = Console.ReadLine();
+
+                for (int i = 0; i < slotNumber.GetLength(0); i++)
+                {
+                    for (int j = 0; j < slotNumber.GetLength(1); j++)
+                    {
+                        if (singleSlotNumber < 9)
+                        {
+                            slotNumber[i, j] = rnd.Next(0, 4);
+                            Console.Write("{0}\t", slotNumber[i, j]);
+                            singleSlotNumber++;
+                        }
+                    }
+                    Console.Write("\n\n");
+                }
+                while (lines == 1)
+                {
+                    if (slotNumber[1, 0] == slotNumber[1, 1] && slotNumber[1, 0] == slotNumber[1, 2])
+                    {
+                        Console.WriteLine($"YOU WIN! You won ${lines * bet} and have ${(cash) + lines * bet} left");
+                        break;
+                    }
+                    else Console.WriteLine($"You lost ${lines * bet} and have ${(cash) - lines * bet} left.");
                     break;
                 }
-            }
-            if (lines > 1)
-            {
-                cash -= multiplier(lines, bet);
-            }
-            else cash -= bet;
-            Console.WriteLine($"You are playing {lines} {(lines > 1 ? "lines" : "line")} and betting ${bet} per line for a total of ${lines * bet}. Press Enter To Spin... ");
-            string keyChoice = Console.ReadLine();
-
-            for (int i = 0; i < slotNumber.GetLength(0); i++)
-            {
-                for (int j = 0; j < slotNumber.GetLength(1); j++)
+                while (lines == 2)
                 {
-                    if (singleSlotNumber < 9)
+                    if (slotNumber[1, 0] == slotNumber[1, 1] && slotNumber[1, 0] == slotNumber[1, 2] && slotNumber[0, 0] == slotNumber[0, 1] && slotNumber[0, 0] == slotNumber[0, 2])
                     {
-                        slotNumber[i, j] = rnd.Next(0, 9);
-                        Console.Write("{0}\t", slotNumber[i, j]);
-                        singleSlotNumber++;
+                        Console.WriteLine($"YOU WIN! You won ${lines * bet} and have ${(cash) + lines * bet} left");
+                        break;
                     }
+                    else Console.WriteLine($"You lost ${lines * bet} and have ${(cash) - lines * bet} left.");
+                    break;
                 }
-                Console.Write("\n\n");
+                while (lines == 3)
+                {
+                    if (slotNumber[1, 0] == slotNumber[1, 1] && slotNumber[1, 0] == slotNumber[1, 2] && slotNumber[0, 0] == slotNumber[0, 1] && slotNumber[0, 0] == slotNumber[0, 2] && slotNumber[2, 0] == slotNumber[2, 1] && slotNumber[2, 0] == slotNumber[2, 2])
+                    {
+                        Console.WriteLine($"YOU WIN! You won ${lines * bet} and have ${(cash) + lines * bet} left");
+                        break;
+                    }
+                    else Console.WriteLine($"You lost ${lines * bet} and have ${(cash) - lines * bet} left.");
+                    break;
+                }
+                while (lines == 4)
+                {
+                    if (slotNumber[1, 0] == slotNumber[1, 1] && slotNumber[1, 0] == slotNumber[1, 2] && slotNumber[0, 0] == slotNumber[0, 1] && slotNumber[0, 0] == slotNumber[0, 2] && slotNumber[2, 0] == slotNumber[2, 1] && slotNumber[2, 0] == slotNumber[2, 2] && slotNumber[0, 0] == slotNumber[1, 1] && slotNumber[0, 0] == slotNumber[2, 2])
+                    {
+                        {
+                            Console.WriteLine($"YOU WIN! You won ${lines * bet} and have ${(cash) + lines * bet} left");
+                            break;
+                        }
+                    }
+                    else Console.WriteLine($"You lost ${lines * bet} and have ${(cash) - lines * bet} left.");
+                    break;
+                }
+                while (lines == 5)
+                {
+                    if (slotNumber[1, 0] == slotNumber[1, 1] && slotNumber[1, 0] == slotNumber[1, 2] && slotNumber[0, 0] == slotNumber[0, 1] && slotNumber[0, 0] == slotNumber[0, 2] && slotNumber[2, 0] == slotNumber[2, 1] && slotNumber[2, 0] == slotNumber[2, 2] && slotNumber[0, 0] == slotNumber[1, 1] && slotNumber[0, 0] == slotNumber[2, 2] && slotNumber[2, 0] == slotNumber[1, 1] && slotNumber[2, 0] == slotNumber[0, 2])
+                    {
+                        {
+                            Console.WriteLine($"YOU WIN! You won ${lines * bet} and have ${(cash) + lines * bet} left");
+                            break;
+                        }
+                    }
+                    else Console.WriteLine($"You lost ${lines * bet} and have ${(cash) - lines * bet} left.");
+                    break;
+                }
+                while (lines == 6)
+                {
+                    if (slotNumber[1, 0] == slotNumber[1, 1] && slotNumber[1, 0] == slotNumber[1, 2] && slotNumber[0, 0] == slotNumber[0, 1] && slotNumber[0, 0] == slotNumber[0, 2] && slotNumber[2, 0] == slotNumber[2, 1] && slotNumber[2, 0] == slotNumber[2, 2] && slotNumber[0, 0] == slotNumber[1, 1] && slotNumber[0, 0] == slotNumber[2, 2] && slotNumber[2, 0] == slotNumber[1, 1] && slotNumber[2, 0] == slotNumber[0, 2] && slotNumber[0, 0] == slotNumber[1, 0] && slotNumber[0, 0] == slotNumber[2, 0])
+                    {
+                        {
+                            Console.WriteLine($"YOU WIN! You won ${lines * bet} and have ${(cash) + lines * bet} left");
+                            break;
+                        }
+                    }
+                    else Console.WriteLine($"You lost ${lines * bet} and have ${(cash) - lines * bet} left.");
+                    break;
+                }
+                while (lines == 7)
+                {
+                    if (slotNumber[1, 0] == slotNumber[1, 1] && slotNumber[1, 0] == slotNumber[1, 2] && slotNumber[0, 0] == slotNumber[0, 1] && slotNumber[0, 0] == slotNumber[0, 2] && slotNumber[2, 0] == slotNumber[2, 1] && slotNumber[2, 0] == slotNumber[2, 2] && slotNumber[0, 0] == slotNumber[1, 1] && slotNumber[0, 0] == slotNumber[2, 2] && slotNumber[2, 0] == slotNumber[1, 1] && slotNumber[2, 0] == slotNumber[0, 2] && slotNumber[0, 0] == slotNumber[1, 0] && slotNumber[0, 0] == slotNumber[2, 0])
+                    {
+                        {
+                            Console.WriteLine($"YOU WIN! You won ${lines * bet} and have ${(cash) + lines * bet} left");
+                            break;
+                        }
+                    }
+                    else Console.WriteLine($"You lost ${lines * bet} and have ${(cash) - lines * bet} left.");
+                    break;
+                }
+                if (cash > 0)
+                    Console.WriteLine("Continue playing? Press Y to play again or N to stop.");
             }
+
         }
 
     }
-
 }
+
+
 
